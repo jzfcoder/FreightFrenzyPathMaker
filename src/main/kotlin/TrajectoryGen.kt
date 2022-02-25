cca76fb26f8c79b44083dfbe1f68a5344c08c119
Fred Co <jzflint@gmail.com>
2022-02-25 13:26:52 -0800

    first commit

---
+++ src/main/kotlin/TrajectoryGen.kt
@@ -0,0 +1,39 @@
+import com.acmerobotics.roadrunner.geometry.Pose2d
+import com.acmerobotics.roadrunner.geometry.Vector2d
+import com.acmerobotics.roadrunner.trajectory.Trajectory
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder
+import com.acmerobotics.roadrunner.trajectory.constraints.DriveConstraints
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumConstraints
+
+object TrajectoryGen {
+    // Remember to set these constraints to the same values as your DriveConstants.java file in the quickstart
+    private val driveConstraints = DriveConstraints(60.0, 60.0, 0.0, 270.0.toRadians, 270.0.toRadians, 0.0)
+
+    // Remember to set your track width to an estimate of your actual bot to get accurate trajectory profile duration!
+    private const val trackWidth = 11.0
+
+    private val combinedConstraints = MecanumConstraints(driveConstraints, trackWidth)
+
+    private val startPose = Pose2d(-62.8, -62.8, 0.0.toRadians)
+
+    fun createTrajectory(): ArrayList<Trajectory> {
+        val list = ArrayList<Trajectory>()
+
+        val builder1 = TrajectoryBuilder(startPose, startPose.heading, combinedConstraints)
+
+        // Small Example Routine
+        builder1
+            .splineTo(Vector2d(50.0, -62.8), 0.0)
+            .splineTo(Vector2d(-12.0, -62.8), 0.0);
+
+        list.add(builder1.build())
+
+        return list
+    }
+
+    fun drawOffbounds() {
+        //GraphicsUtil.fillRect(Vector2d(0.0, -63.0), 18.0, 18.0) // robot against the wall
+    }
+}
+
+val Double.toRadians get() = (Math.toRadians(this))
